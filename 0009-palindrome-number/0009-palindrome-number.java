19081fc75438b9cf4ab64ebbfee45e974b4911cc
Anushka Sharma <120203050+anuh02@users.noreply.github.com>
2025-01-06 23:19:17 +0530

    Time: 5 ms (71.62%), Space: 44.5 MB (10.3%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x < 0)
+        return false;
         return (x == reverse(x)) ;
     }
 
     public int reverse(int a)
     {
         
-        int rev = 1;
+        int rev = 0;
         int rem = 0;
-        while(a >= 0)
+        while(a != 0)
         {
             rem = a % 10;
             rev = rev * 10 + rem;
